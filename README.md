3ab6d5e2924b6583d61c92f4d9fa065d8d4365b8
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-11-05 00:49:09 +0600

    Add the basic project structure with necessary folders and routes

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Community Food Sharing Assignment
 
+Client side GitHub link: 
+https://github.com/Porgramming-Hero-web-course/b8a11-client-side-sifurr
+
+
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from "react-router-dom";
+import index from './routes';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <RouterProvider router={index} />
   </React.StrictMode>,
 )
